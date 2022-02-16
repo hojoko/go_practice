7d43f3a7a534ad0d2c9afbfecc6b4f629ccd6061
hojoko <hojoko03@gmail.com>
2022-02-17 01:08:35 +0900

    初期値の設定

---
+++ variable.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main(){
+	var num1 int = 1
+	fmt.Println(num1)
+
+	var num2 = 2
+	fmt.Println(num2)
+
+	//初期値の設定は主にコロンイコールで行われる
+	num3 := 3
+	fmt.Println(num3)
+}
